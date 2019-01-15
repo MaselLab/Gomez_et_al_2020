85cc7935b7f1ba935f9e5b56c338f912fd0f8826
Kevin Gomez <kgomez81@math.arizona.edu>
2019-01-15 15:20:55 -0700

    new file added

---
+++ figure_compareV.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 15 14:09:05 2019
+
+@author: dirge
+"""
+
+from mpl_toolkits.mplot3d import Axes3D
+from scipy.stats import multivariate_normal
+from numpy import inf
+import matplotlib.ticker as mtick
+import pickle
+import matplotlib.pyplot as plt
+import scipy as sp
+import numpy as np
+import matplotlib.mlab as mlab
+import plotfunctions as pltfun
+
+
+# load processed matlab data for figure
+# -----------------------------------------------------------------------------
+
+# create heatmap of v reduction
+# -----------------------------------------------------------------------------
