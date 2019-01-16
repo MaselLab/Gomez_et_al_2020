d723e40df64e5a0b1d7e06209061295f45b26363
Kevin <kgomez81@math.arizona.edu>
2019-01-15 18:04:45 -0700

    Created preliminary figure comparing adaptation rates of two traits for various choices of s and U.

--- figure_compareV.py
+++ figure_compareV.py
@@ -9,16 +9,38 @@ from mpl_toolkits.mplot3d import Axes3D
 from scipy.stats import multivariate_normal
 from numpy import inf
 import matplotlib.ticker as mtick
+
 import pickle
 import matplotlib.pyplot as plt
 import scipy as sp
 import numpy as np
 import matplotlib.mlab as mlab
-import plotfunctions as pltfun
 
 
 # load processed matlab data for figure
 # -----------------------------------------------------------------------------
+pickle_file_name = 'fig_compareVdata-01.pickle'
+pickle_file = open(pickle_file_name,'wb') 
+pickle.dump([N,s,U,v,parameters,grand_means,sarry,Uarry,v1_data,v2_data],pickle_file,pickle.HIGHEST_PROTOCOL)
+pickle_file.close()
+
+v_comp = np.log10(v1_data/v2_data)
+arry_dim = len(sarry)
+my_xlabel = [str(np.round(np.log10(sarry[i,0]),1)) for i in range(len(sarry))]
+my_ylabel = [str(np.round(np.log10(Uarry[i,0]),1)) for i in range(len(Uarry))]
 
 # create heatmap of v reduction
 # -----------------------------------------------------------------------------
+fig1, ax1 = plt.subplots(1,1,figsize=[10,8])
+fit_distr_2d = ax1.pcolormesh(v_comp.transpose(),cmap=plt.cm.gray_r)
+cbar = plt.colorbar(fit_distr_2d)
+ax1.axis('tight')        
+ax1.set_xticks(np.arange(arry_dim)+0.5)
+ax1.set_yticks(np.arange(arry_dim)+0.5)        
+ax1.set_xticklabels(my_xlabel)
+ax1.set_yticklabels(my_ylabel)        
+ax1.set_xlabel('Trait 1 (s Log10 Scale)',fontsize=18,labelpad=20)
+ax1.set_ylabel('Trait 2 (U Log10 Scale)',fontsize=18,labelpad=10)
+ax1.tick_params(axis='both',labelsize=14)        
+cbar.ax.text(2.5,0.65,'Log$_{10}$ of $v_1/v_2$',rotation=90,fontsize=18)
+fig1.savefig('fig_compareVdata.pdf')
