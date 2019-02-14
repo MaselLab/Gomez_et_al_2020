3fd7dacd4474eb3059a758eb51f99bd193a177a0
Kevin <kgomez81@math.arizona.edu>
2019-02-13 17:21:47 -0700

    further updates for phase plane.

--- fig_sUtradeoff.py
+++ fig_sUtradeoff.py
@@ -29,6 +29,30 @@ v0=myfun.get_v(N0,s0,U0)
 [U_min,U_max] = [1e-13, 5e-3]
 no_div = 100
 
+def f(Y, t):
+    y1, y2 = Y
+    return [y2, -np.sin(y1)]
+
+y1 = np.linspace(-2.0, 8.0, 20)
+y2 = np.linspace(-2.0, 2.0, 20)
+Y1, Y2 = np.meshgrid(y1, y2)
+t = 0
+u, v = np.zeros(Y1.shape), np.zeros(Y2.shape)
+NI, NJ = Y1.shape
+for i in range(NI):
+    for j in range(NJ):
+        x = Y1[i, j]
+        y = Y2[i, j]
+        yprime = f([x, y], t)
+        u[i,j] = yprime[0]
+        v[i,j] = yprime[1]
+Q = plt.quiver(Y1, Y2, u, v, color='r')
+plt.xlabel('$y_1$')
+plt.ylabel('$y_2$')
+plt.xlim([-2, 8])
+plt.ylim([-4, 4])
+
+
 s = [(s_min*10**(i*np.log10(s_max/s_min)/no_div)) for i in range(no_div+1)]
 
 Us = [v0/(N0*s[i]**2) for i in range(no_div+1)]
