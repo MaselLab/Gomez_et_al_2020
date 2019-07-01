9f9e94a485a84abc5d79722967a96b1c15d291f6
Kevin <kgomez81@math.arizona.edu>
2019-07-01 15:24:43 -0700

    Modified figure that compares v's of two traits.

--- figure_compareV.py
+++ figure_compareV.py
@@ -26,9 +26,9 @@ import matplotlib.mlab as mlab
 # set file name of data
 # -----------------------------------------------------------------------------
 
-#pickle_file_name = 'fig_compareVdata-16.pickle'    # data for comparison with fixed v
+pickle_file_name = 'fig_compareVdata-16.pickle'    # data for comparison with fixed v
 #pickle_file_name = 'fig_compareVdata-19.pickle'    # data for comparison with fixed R
-pickle_file_name = 'fig_compareVdata-20.pickle'    # data for comparison with fixed R
+#pickle_file_name = 'fig_compareVdata-20.pickle'    # data for comparison with fixed R
 
 # load processed matlab data for figure
 # -----------------------------------------------------------------------------
@@ -40,21 +40,27 @@ pickle_file.close()
 # comment out lines depending on R or v (these sections indicated by ****)
 
 rate_comp = v1_data/v2_data
+rate_comp = np.transpose(rate_comp[::-1])[::-1]
+
 [m,n] = rate_comp.shape
 [m,n] = [int(m),int(n)]
 
-for i in range(n-1):                # remove upper portion of grid 
+for i in range(n-1):                # remove lower portion of grid
     for j in range(m-1-i):
         rate_comp[i,m-1-j]= 1
 
 for i in range(n):                  #set R1/R2 or v1/v2 < 1 to 1 for cbar map correction  
     for j in range(m-i):
 # *****************************************************************************        
-        rate_comp[i+j,i]=rate_comp[i+j,i]*(sarry[i]/sarry[i+j])
+#        rate_comp[i+j,i]=rate_comp[i+j,i]*(sarry[i]/sarry[i+j])
 # *****************************************************************************
         if (rate_comp[i+j,i]< 1):
-            rate_comp[i+j,i]=1
-        
+            rate_comp[i+j,i]=2-rate_comp[i+j,i]
+
+# flip the entries to get right matching of values with ordinal axis U vs. s
+for i in range(n):
+    rate_comp[i] = rate_comp[i][::-1]
+
 arry_dim = len(sarry)
 my_slabel = ['$10^{'+str(np.round(np.log10(sarry[i,0]),1))+'}$' for i in range(len(sarry))]
 my_Ulabel = ['$10^{'+str(np.round(np.log10(Uarry[i,0]),1))+'}$' for i in range(len(Uarry))]
@@ -78,7 +84,7 @@ fig1, ax1 = plt.subplots(1,1,figsize=[15,10])
 #fit_distr_2d = ax1.pcolormesh(rate_comp.transpose(),cmap=plt.cm.gray_r)
 #cbar = plt.colorbar(fit_distr_2d,pad = 0.15,ticks=[1.0+.1*i for i in range(13)])
 
-fit_distr_2d = ax1.pcolormesh(np.log10(rate_comp.transpose()),cmap=plt.cm.gray_r)
+fit_distr_2d = ax1.pcolormesh(rate_comp,cmap=plt.cm.gray_r)
 cbar = plt.colorbar(fit_distr_2d,pad = 0.15)
 # *****************************************************************************
 
@@ -87,28 +93,28 @@ ax1.axis('tight')
 ax1.set_xticks(np.arange(arry_dim)+0.5)
 ax1.set_yticks(np.arange(arry_dim)+0.5)        
 ax1.set_xticklabels(my_slabel)
-ax1.set_yticklabels(my_slabel)        
+ax1.set_yticklabels(my_Ulabel[::-1])        
 ax1.set_xlabel('Selection coefficient trait 1',multialignment='center',fontsize=18,labelpad=10)
-ax1.set_ylabel('Selection coefficient trait 2',multialignment='center',fontsize=18,labelpad=10)
+ax1.set_ylabel('Mutation rate trait 2',multialignment='center',fontsize=18,labelpad=10)
 ax1.tick_params(axis='both',labelsize=12)
 # *****************************************************************************
-cbar.ax.text(2.8,0.6,'Ratio of $R_1/R_2$',rotation=270,fontsize=22)     # use this label of comparing R
-#cbar.ax.text(2.8,0.6,'Ratio of $v_1/v_2$',rotation=270,fontsize=22)    # use this label of comparing v
+#cbar.ax.text(2.8,0.6,'Ratio of $R_1/R_2$',rotation=270,fontsize=22)     # use this label of comparing R
+cbar.ax.text(2.8,0.6,'Ratio of $v_1/v_2$',rotation=270,fontsize=22)    # use this label of comparing v
 # *****************************************************************************
 
-plt.text(1,m-1,r'$N = 10^9$',fontsize=18)
+plt.text(1.5,2,r'$N = 10^9$',fontsize=18)
 # *****************************************************************************
-plt.text(1,m-1.7,r'$R = 5.3\times 10^{-3}$',fontsize=18)     # use this label of comparing R
-#plt.text(1,m-1.7,r'$v = 5.3\times 10^{-5}$',fontsize=18)     # use this label of comparing v
+#plt.text(1,m-1.7,r'$R = 5.3\times 10^{-3}$',fontsize=18)     # use this label of comparing R
+plt.text(1.5,1,r'$v = 5.3\times 10^{-5}$',fontsize=18)     # use this label of comparing v
 # *****************************************************************************
 
 #ax1.add_patch(Rectangle((someX - 0.1, someY - 0.1), 0.2, 0.2, alpha=1, facecolor='none'))
 
 ax2 = ax1.twinx()
 ax2.yaxis.set_ticks(np.arange(0+0.5/(len(my_Ulabel)),1+0.5/(len(my_Ulabel)),1.0/(len(my_Ulabel))))
-ax2.set_yticklabels(my_Ulabel)
+ax2.set_yticklabels(my_slabel[::-1])
 ax2.tick_params(labelsize=12)
-ax2.set_ylabel('Mutation rate trait 2',fontsize=18,labelpad=20,rotation=270)
+ax2.set_ylabel('Selection coefficient trait 2',fontsize=18,labelpad=20,rotation=270)
 
 ax3 = ax1.twiny()
 ax3.xaxis.set_ticks(np.arange(0+0.5/(len(my_Ulabel)),1+0.5/(len(my_Ulabel)),1.0/(len(my_Ulabel))))
@@ -120,7 +126,7 @@ ax3.set_xlabel('Mutation rate trait 1',fontsize=18,labelpad=20)
 fig1.subplots_adjust(bottom=0.2,left=0.2)
 
 plt.text(.21,-2.1,'Trait 1 favored by selection',fontsize=22)
-plt.text(-0.185,11.5,'Trait 2 favored by mutation',rotation=90,fontsize=22)
+plt.text(-0.19,11.5,'Trait 2 favored by mutation',rotation=90,fontsize=22)
         
 # color x axis
 ax1.annotate("",
@@ -143,21 +149,21 @@ ax1.annotate("",
             
 # color y axis
 ax1.annotate("",
-            xy=(-1.4,0), xycoords='data',
-            xytext=(-1.4,5), textcoords='data',
-            arrowprops=dict(arrowstyle="-",connectionstyle="arc3",color='lime',lw=5),
+            xy=(-1.5,0), xycoords='data',
+            xytext=(-1.5,5), textcoords='data',
+            arrowprops=dict(arrowstyle="-",connectionstyle="arc3",color='cyan',lw=5),
             annotation_clip=False)
 
 ax1.annotate("",
-            xy=(-1.4,5), xycoords='data',
-            xytext=(-1.4,10), textcoords='data',
+            xy=(-1.5,5), xycoords='data',
+            xytext=(-1.5,10), textcoords='data',
             arrowprops=dict(arrowstyle="-",connectionstyle="arc3",color='yellow',lw=5),
             annotation_clip=False)
 
 ax1.annotate("",
-            xy=(-1.4,10), xycoords='data',
-            xytext=(-1.4,15), textcoords='data',
-            arrowprops=dict(arrowstyle="-",connectionstyle="arc3",color='cyan',lw=5),
+            xy=(-1.5,10), xycoords='data',
+            xytext=(-1.5,15), textcoords='data',
+            arrowprops=dict(arrowstyle="-",connectionstyle="arc3",color='lime',lw=5),
             annotation_clip=False)
             
-fig1.savefig('figures/fig_two_trait_compare_v_poster.pdf',bbox_inches='tight')
+fig1.savefig('figures/fig_two_trait_compare_v.pdf',bbox_inches='tight')
