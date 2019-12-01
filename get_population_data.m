8a16ae79709f03be30463d38dc90312cd40548b8
Kevin <kgomez81@math.arizona.edu>
2019-11-30 18:04:07 -0700

    new file to allow simulations to continue from prior ones. This function gets prior outputs and turns them into matlab data types that can be used to initialize simulations starting where prior ones finished.

---
+++ get_population_data.m
@@ -0,0 +1,69 @@
+function [adj_time,param,pop,fit,fitx,fity] = get_population_data(input_file_param,input_file_class,input_file_abund,input_file_summr)
+% get_population_data take ending distribution data from a previous simlation and
+% recreates the arrays to use in a new simulation that will continue the
+% prior one.
+
+% input files:
+% -input_file_param = set of parameters used in prior simulation
+% -input_file_class = set of classes at end of prior simulation
+% -input_file_abund = set of abundances at end of prior simulation
+% output files:
+% -param = array of parameters from prior simulation
+% -pop = array of abundances at end of prior simulation
+% -fit = array of abundances at end of prior simulation
+% -fitx = array of abundances at end of prior simulation
+% -fity = array of abundances at end of prior simulation
+
+    pop = 0;
+    fit = 0;
+    fitx = 0;
+    fity = 0;
+
+    % open files holding population data from a prior simulation
+    fileID1 = fopen(input_file_param,'r');
+    fileID2 = fopen(input_file_class,'r');
+    fileID3 = fopen(input_file_abund,'r');
+    fileID4 = fopen(input_file_summr,'r');
+    
+    % reading data from prior simulation
+    param_data = fgetl(fileID1);
+    class_data = fgetl(fileID2); 
+        class_data = class_data(1:end-1);
+        class_data = replace(class_data,"],[",";")
+    abund_data = fgetl(fileID3); 
+        abund_data = abund_data(1:end-1);
+    summr_data = fgetl(fileID4); 
+        
+    fclose(fileID1);
+    fclose(fileID2);
+    fclose(fileID3);
+    fclose(fileID4);
+    
+    eval(['param = [' param_data '];'])
+    eval(['temp_class = ' class_data ';'])
+    eval(['temp_abund = [' abund_data '];'])
+    eval(['temp_summr = ' summr_data ';'])
+    
+    % set time adjustment
+    adj_time = temp_summr(1,1);
+    
+    % build fitness arrays
+    s1 = temp_param(1,2);
+    s2 = temp_param(1,4);
+    fitx = min(temp_class(:,1)):1:max(temp_class(:,1));
+    fity = min(temp_class(:,2)):1:max(temp_class(:,2));
+    dim = [length(fitx) length(fity)];
+    fitx_arry = s1*fitx'*ones(1,dim(2));           
+    fity_arry = s2*ones(dim(1),1)*fity;            
+    fit = fitx_arry + fity_arry;                   
+    
+    % build array with abundances
+    pop = zeros(dim(1),dim(2));
+    
+    for i=1:length(temp_class)
+        i1 = temp_class(i,1)-min(fitx)+1;
+        i2 = temp_class(i,2)-min(fity)+1;
+        pop(i1,i2) = temp_abund(i);
+    end
+    
+end
